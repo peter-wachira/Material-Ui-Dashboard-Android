bc1853e72f1f8d594bf9435458f098b517520e4b
Wazinsure PC <you@example.com>
2019-08-19 15:50:02 +0300

    add card onclick listeners

---
+++ app/src/main/java/com/wazinsure/homedashboard/Links.java
@@ -0,0 +1,14 @@
+package com.wazinsure.homedashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Links extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_links);
+    }
+}
--- app/src/main/java/com/wazinsure/homedashboard/MainActivity.java
+++ app/src/main/java/com/wazinsure/homedashboard/MainActivity.java
@@ -1,14 +1,52 @@
 package com.wazinsure.homedashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private CardView bankCard, ideaCard, addCard, linksCard, wifiCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //defining cards
+        bankCard = (CardView) findViewById(R.id.bank_card);
+        ideaCard = (CardView) findViewById(R.id.idea_card);
+        addCard = (CardView) findViewById(R.id.add_card);
+        linksCard = (CardView) findViewById(R.id.links_card);
+        wifiCard = (CardView) findViewById(R.id.wifi_card);
+
+        //Add click listener to the cards
+        bankCard.setOnClickListener(this);
+        ideaCard.setOnClickListener(this);
+        addCard.setOnClickListener(this);
+        linksCard.setOnClickListener(this);
+        wifiCard.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent i;
+
+        switch (v.getId()) {
+
+            case R.id.bank_card: i = new Intent(this,Bank.class);startActivity(i);break;
+            case R.id.idea_card :i= new Intent(this, Ideas.class);startActivity(i);break;
+            case R.id.add_card: i= new Intent(this,Add.class);startActivity(i);break;
+            case R.id.wifi_card: i= new Intent(this,Wifi.class);startActivity(i);break;
+            case R.id.links_card: i= new Intent(this,Links.class);startActivity(i);break;
+
+            default:break;
+
+
+        }
     }
 }
